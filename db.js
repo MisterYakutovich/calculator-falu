baf5d9947fe9f6df75e6d947640ef13367da87dd
Pavel <pashtet.net5@gmail.com>
2024-11-18 22:09:07 +0300

    refactor:add getDialectModule

--- db.js
+++ db.js
@@ -2,6 +2,10 @@ import {Sequelize} from "sequelize"
 import dotenv from 'dotenv';
 
 dotenv.config();
+const getDialectModule = async () => {
+   const { default: mysql2 } = await import('mysql2');
+   return mysql2;
+  };
 
 export const sequelize=  new Sequelize(
    process.env.DB_NAME, 
@@ -9,6 +13,7 @@ export const sequelize=  new Sequelize(
    process.env.DB_PASSWORD, 
    {
    dialect: 'mysql',
+   dialectModule: await getDialectModule(),
    logging: console.log,
    host:process.env.DB_HOST,
    port:process.env.DB_PORT,
