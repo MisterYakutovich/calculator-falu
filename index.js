7a15c4cc298e5609bd5e1f47b0b0aaf1e34afa1c
Pavel <pashtet.net5@gmail.com>
2024-11-18 21:36:47 +0300

    refactor:chanched_3 vercel.json

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { SemiFinishedProduct, Packaging, Defect } from './models/models.js';
 
 dotenv.config();
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
  app.use(cors());
  app.use(express.json())
  app.use(bodyParser.json());
