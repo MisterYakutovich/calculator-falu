223f26a1b7a7420b0b6f862b5ef77cd6139a3ce5
Pavel <pashtet.net5@gmail.com>
2024-11-21 12:14:28 +0300

    refactor:optimized connection db to vercel

--- db.js
+++ db.js
@@ -12,9 +12,8 @@ export const sequelize=  new Sequelize(
    process.env.DB_USER, 
    process.env.DB_PASSWORD, 
    {
+   host:process.env.DB_HOST,
    dialect: 'mysql',
    dialectModule: await getDialectModule(),
    logging: console.log,
-   host:process.env.DB_HOST,
-   port:process.env.DB_PORT,
    });
