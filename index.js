e199ce5c948e44bb83dd82de5b114aa532737f90
Pavel <pashtet.net5@gmail.com>
2024-11-24 12:15:27 +0300

    refactor:add new key for cors in vercel.json

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ import { SemiFinishedProduct, Packaging, Defect } from './models/models.js';
 dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 5000
- app.use(cors({
-  origin: '*', // Замените на ваш домен
-  methods: ['GET', 'POST'], // Разрешенные методы
-  allowedHeaders: ['Content-Type'], // Разрешенные заголовки
-}));
+ app.use(cors());
  app.use(express.json())
  app.use(bodyParser.json());
  app.use(controller);
