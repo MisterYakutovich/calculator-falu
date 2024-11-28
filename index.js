f44adb5e3f181d7c750bdc3547e7ab66ddc63fc5
Pavel <pashtet.net5@gmail.com>
2024-11-28 20:31:56 +0300

    test3 cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
  app.use(cors(
   {
-    origin: 'http://localhost:5501/calculator-falu/pages/index.html',
+    origin: 'http://localhost:5501',
     
 }
  ));
